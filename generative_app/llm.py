57549cbcd924097e884392c223885c4516a104cb
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-09-16 14:06:09 +0530

    Update

--- generative_app/llm.py
+++ generative_app/llm.py
@@ -19,13 +19,18 @@ python_script = os.path.join(os.getcwd() , "langchain" ,"generated_script.py")
 
 template = """You're an AI assistant specializing in python development. Based on the input provided, you must update the python code that is compatible with python 3.9. Additionally, offer a brief explanation about how you arrived at the python code.
 If the input is a question, you must explain the code only and additionnaly propose some code. Do not halucinate or make up information. If you do not know the answer, just say "I don't know".
+
 The python code you must update is the following:
 ```python
 {python_code}
 ```
+
 Do not forget about importing the necessary libraries.
+
 Write your anwser in the following format: the code and explanation in markdown format
+
 Question: {question}
+
 Answer:"""
 
 def setup(openai_api_key: str):
@@ -38,11 +43,6 @@ def parse(output):
     python_code = None
     explain_code = None
     python_code_match = re.search(r"```python\n(.*?)\n```\n(.*)", output, re.DOTALL)
-    if python_code_match:
-        python_code = python_code_match.group(1)
-        explain_code = python_code_match.group(2)
-        print(python_code)
-        print(explain_code)
-    else:
-        print("Python code not found.")
+    python_code = python_code_match.group(1)
+    explain_code = python_code_match.group(2)
     return python_code, explain_code
