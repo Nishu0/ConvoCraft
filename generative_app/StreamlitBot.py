e844c0696209fc83812bce6aa0c2b15fd37ac411
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-09-19 22:42:43 +0530

    Hydra and Pages

--- generative_app/StreamlitBot.py
+++ generative_app/StreamlitBot.py
@@ -1,37 +1,43 @@
-import warnings
 import os
 import streamlit as st
-
+import uuid
 import sidebar
-import chat
-import llm
+from hydralit import HydraHeadApp
 
-warnings.filterwarnings("ignore")
+from chat import ChatBot
 
 
-st.set_page_config(
-    page_title="ConvoCraft",
-    page_icon="🤖",
-    layout="centered",
-    initial_sidebar_state=sidebar.sidebar_init_state,
-    menu_items={
-        "Report a bug": "https://github.com/Nishu0/ConvoCraft/issues",
-        "About": """
-            # ConvoCraft
-            Transform conversations into stunning web apps. Dynamic code generation + intuitive interface. Unleash your creativity effortlessly. Use the power of GPT OpenAI LLM and Langchain.
+class ChatBotApp(HydraHeadApp):
+    """
+    This is an example signup application to be used to secure access within a HydraApp streamlit application.
 
-            Go to the GitHub repo to learn more about the project. https://github.com/Nishu0/ConvoCraft
-            """,
-    },
-)
+    This application is an example of allowing an application to run from the login without requiring authentication.
 
-sidebar.setup()
+    """
 
-openai_api_key = st.secrets["openai_api_key"]
+    def __init__(self, title = '', **kwargs):
+        self.__dict__.update(kwargs)
+        self.title = title
 
-if openai_api_key:
-    assistant = llm.setup(openai_api_key)
-    generative_app_path = os.path.join(os.getcwd() , "generative_app", "pages" ,"🤖GeneratedApp.py")
-    chat.setup(assistant, generative_app_path)
 
-st.markdown('<div id="input-container-placeholder"></div>', unsafe_allow_html=True)
+    def run(self) -> None:
+        sidebar.setup()
+
+        generative_app_path = os.path.join(os.getcwd(), ".generated_apps", "generated_app.py")
+
+        # Generate uuid
+        if "client_id" not in st.session_state:
+            st.session_state["client_id"] = str(uuid.uuid4())
+
+        # Check if it's the first run
+        st.session_state["first_run"] = "first_run" not in st.session_state
+
+
+        #if time_sandbox.setup():
+        chat = ChatBot(generative_app_path)
+        chat.setup(st.session_state["first_run"])
+
+        st.markdown('<div id="input-container-placeholder"></div>', unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    ChatBotApp().run()
